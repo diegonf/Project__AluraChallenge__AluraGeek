d446874b7ad68f482eb6c89054d00ecfe658586b
Diego Ferreira <diego.nascimento91@gmail.com>
2022-04-06 11:14:03 -0300

    alterado modo de renderizar header e footer. Criado arquivo html para cada um e criado scrip js para renderizar em cada pagina. Foi aterado também organização das pastas. Criado uma pasta assets para todas as imgs e css e js compartilhados, alem dos arquivos footer e header html. E foi criado uma pasta para cada pagina (adicionar, produto, produtos, home e login) com um arquivo index.html dentro

---
+++ assets/main.js
@@ -0,0 +1,16 @@
+(function () {
+  const header = document.querySelector('[data-header]');
+  const url = '../assets/header.html';
+  fetch(url)
+  .then (res => res.text())
+  .then (data => {
+        header.innerHTML = data;
+  });
+  const footer = document.querySelector('[data-footer]');
+  const url2 = '../assets/footer.html';
+  fetch(url2)
+  .then (res => res.text())
+  .then (data => {
+        footer.innerHTML = data;
+  });
+}());
--- main.js
+++
@@ -1,8 +0,0 @@
-import { CallGetData } from "./js/getDataAPI.js";
-import { CallImgDrop } from "./js/imgDrop.js";
-
-//drag and drop function
-// CallImgDrop();
-
-//API to get data
-CallGetData();
