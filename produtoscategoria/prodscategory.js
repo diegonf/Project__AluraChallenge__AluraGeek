88de7250b4bc9bf12fa0c7f7f3d22362da0984fb
Diego Ferreira <diego.nascimento91@gmail.com>
2022-04-10 16:43:23 -0300

    refatorado codigo

--- produtoscategoria/prodscategory.js
+++ produtoscategoria/prodscategory.js
@@ -1,24 +1,22 @@
 import { apiGetData } from "../assets/js/getDataAPI.js";
 
-const preLoadProdsCategory = () => {
-  const treatData = (productList) => {
-    const categoryTitle = document.querySelector('[data-categorytitle]');
-    const prodsCategory = document.querySelector('[data-prodscategory]');
-    let activeCategory = localStorage.getItem('activecategory');
-    if (activeCategory === null || activeCategory === ''){
-      activeCategory = 'Star Wars';
-      localStorage.setItem('activecategory', activeCategory);
-    }
 
-    productList.forEach((prod, id) => {
-        if (prod.category === activeCategory){
-          prod.addElement(prodsCategory, id);
-          categoryTitle.innerHTML = activeCategory;
-        }
-      
-    });
+const loadProductsCategory = (productList) => {
+  const categoryTitle = document.querySelector('[data-categorytitle]');
+  const prodsCategory = document.querySelector('[data-prodscategory]');
+  let activeCategory = localStorage.getItem('activecategory');
+  if (activeCategory === null || activeCategory === ''){
+    activeCategory = 'Star Wars';
+    localStorage.setItem('activecategory', activeCategory);
   }
-  apiGetData(treatData);
-};
 
-preLoadProdsCategory();
+  productList.forEach((prod, id) => {
+      if (prod.category === activeCategory){
+        prod.addElement(prodsCategory, id);
+        categoryTitle.innerHTML = activeCategory;
+      }
+    
+  });
+}
+
+apiGetData(loadProductsCategory);
