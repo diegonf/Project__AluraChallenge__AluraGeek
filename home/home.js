7f8f29f452a0ed216b0f64f6c783d24055d89add
Diego Ferreira <diego.nascimento91@gmail.com>
2022-04-10 14:50:49 -0300

    alterado arquivo home.js para se adaptar aos novos nomes de categorias e para a funcao getDataAPI com callback.

--- home/home.js
+++ home/home.js
@@ -3,35 +3,38 @@ import { apiGetData } from "../assets/js/getDataAPI.js";
 const starwars = document.querySelector('[data-starwars]');
 const devices = document.querySelector('[data-devices]');
 const others = document.querySelector('[data-others]');
-const productList = apiGetData();
 
-let i = 0, j = 0, k = 0;
-productList.forEach((prod, id) => {
-  if (prod.category == 'starwars') {
-    if ( i >= 4){
-      prod.smallProduct.classList.add('gallery__product--desktop');
+const loadProductsHome = (productList) => {
+  let i = 0, j = 0, k = 0;
+  productList.forEach((prod, id) => {
+    if (prod.category === 'Star Wars') {
+      if ( i >= 4){
+        prod.smallProduct.classList.add('gallery__product--desktop');
+      }
+      starwars.appendChild(prod.smallProduct);
+      i++
     }
-    starwars.appendChild(prod.smallProduct);
-    i++
-  }
-  if (prod.category == 'consoles') {
-    if ( j >= 4){
-      prod.smallProduct.classList.add('gallery__product--desktop');
+    if (prod.category === 'Consoles') {
+      if ( j >= 4){
+        prod.smallProduct.classList.add('gallery__product--desktop');
+      }
+      devices.appendChild(prod.smallProduct);
+      j++
     }
-    devices.appendChild(prod.smallProduct);
-    j++
-  }
-  if (prod.category == 'diversos') {
-    if ( k >= 4){
-      prod.smallProduct.classList.add('gallery__product--desktop');
+    if (prod.category === 'Diversos') {
+      if ( k >= 4){
+        prod.smallProduct.classList.add('gallery__product--desktop');
+      }
+      others.appendChild(prod.smallProduct);
+      k++
     }
-    others.appendChild(prod.smallProduct);
-    k++
-  }
 
-  prod.smallProduct.lastChild.addEventListener('click', (event) => {
-    event.preventDefault();
-    localStorage.setItem('activeproduct', id);
-    window.location.replace("../produto");
+    prod.smallProduct.lastChild.addEventListener('click', (event) => {
+      event.preventDefault();
+      localStorage.setItem('activeproduct', id);
+      window.location.replace("../produto");
+    });
   });
-});
+}
+
+apiGetData(loadProductsHome);
